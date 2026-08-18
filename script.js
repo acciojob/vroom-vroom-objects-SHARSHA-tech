64b9fe4cf225a06e25fdb525305db9e000e58458
SHARSHA-tech <harshashendge@gmail.com>
2026-08-18 21:47:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	
+		this.make=make;
+		this.model=model;
+}
+	getMakeModel(){
+      return this.make +"make model" + this.model;
+		}
 
-function SportsCar(make, model, topSpeed) {}
 
+
+function SportsCar(make, model, topSpeed) {
+ super (make,model);
+	this.topSpeed=topSpeed;
+	getTopSpeed(){
+		return this.topSpeed
+	}
+}
+SportsCar(1000);
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
